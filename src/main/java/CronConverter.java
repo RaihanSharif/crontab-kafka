8844367194ddd88f3751cf26f5f4352b6c82c70b
Raihan Sharif <raihansharif92@gmail.com>
2026-08-23 18:45:43 +0100

    extract out the converter to separate file

---
+++ src/main/java/CronConverter.java
@@ -0,0 +1,28 @@
+import javax.swing.table.JTableHeader;
+
+public class CronConverter {
+
+    public static String toQuartzCron(String unixCron) {
+        String [] fields = unixCron.trim().split("\\s+");
+
+        if (fields.length != 5) {
+            throw new IllegalArgumentException(
+                    "Expected 5 fields in cron expression, got: " + fields.length);
+        }
+        String minute = fields[0];
+        String hour = fields[1];
+        String dayOfMonth = fields[2];
+        String month = fields[3];
+        String dayOfWeek = fields[4];
+
+        if (dayOfMonth.equals("*") && dayOfWeek.equals("*")) {
+            dayOfWeek = "?";
+        } else if (!dayOfMonth.equals("*") && dayOfWeek.equals("*")) {
+            dayOfWeek = "?";
+        } else if (dayOfMonth.equals("*") && !dayOfWeek.equals("*")) {
+            dayOfMonth = "?";
+        }
+
+        return String.join(" ", minute, hour, dayOfMonth, month, dayOfWeek);
+    }
+}
